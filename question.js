9c4ce6ec7a47cbd058504f74f94c8a9a4dbff0bd
techyMk <mani18012003@gmail.com>
2024-11-05 11:28:29 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the time complexity of accessing an element in an array?",
+        answer: "A. O(1)",
+        options: [
+            "A. O(1)",
+            "B. O(n)",
+            "C. O(log n)",
+            "D. O(n^2)"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following sorting algorithms has the best average-case time complexity?",
+        answer: "B. Quick Sort",
+        options: [
+            "A. Bubble Sort",
+            "B. Quick Sort",
+            "C. Insertion Sort",
+            "D. Selection Sort"
+        ]
+    },
+    {
+        numb: 3,
+        question: "In Java, which access modifier allows visibility only within the same package?",
+        answer: "D. No modifier (default access)",
+        options: [
+            "A. public",
+            "B. private",
+            "C. protected",
+            "D. No modifier (default access)"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is a front-end JavaScript framework?",
+        answer: "C. Angular",
+        options: [
+            "A. Django",
+            "B. Flask",
+            "C. Angular",
+            "D. Spring"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which SQL statement is used to retrieve data from a database?",
+        answer: "B. SELECT",
+        options: [
+            "A. GET",
+            "B. SELECT",
+            "C. RETRIEVE",
+            "D. FETCH"
+        ]
+    }
+];
